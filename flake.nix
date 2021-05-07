264532b2507a434b51d4aaf0b3d6815236ec8e0e
Pablo Ovelleiro Corral <github@pablo.tools>
2021-05-07 23:06:28 +0200

    initial commit

---
+++ flake.nix
@@ -0,0 +1,45 @@
+
+{
+  description = "A flake for building Hello World";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-20.03;
+
+  outputs = { self, nixpkgs }: {
+
+    defaultPackage.x86_64-linux =
+      # Notice the reference to nixpkgs here.
+      with import nixpkgs { system = "x86_64-linux"; };
+
+      pkgs.writeScriptBin "cheer" ''
+        cat << EOF
+
+                             /^\
+                   /         (/^\)     /
+              \   ( \         \ /     ( \     /^\
+             / )   \ |        _|_      \ |   |/^\|
+            | /    _|_        | |      _|_    \ /
+            _|_    | |        | |      | |    _|_
+            | |    | |        | |      | |    | |
+            | |    | |    ****| |******| |    | |
+            | |****| |****    | |      | |****| |
+           *| |    | |                 | |    | |*****
+         *  | |   H  A  P  P  Y               | |      *
+        *                                               *
+        | *            B  I  R  T  H  D  A  Y  !      * |
+        |  *****                                 *****  |
+        |@      **********             **********      @|
+        | @   @           *************           @   @ |
+        |  @@@ @    @                       @    @ @@@  |
+        |       @@@@ @      @       @      @ @@@@       |
+         *            @@@@@@ @     @ @@@@@@            *
+          *                   @@@@@                   *
+           *****                                 *****
+                **********             **********
+                          *************
+        EOF
+        '';
+
+      defaultApp.x86_64-linux = { type = "app"; program = "${self.defaultPackage.x86_64-linux }/bin/cheer"; };
+
+  };
+}
